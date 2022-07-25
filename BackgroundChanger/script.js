00aa5b8fd8ef6511374f5caf34cf756b10d93af0
Aditya Anand <adityaanand.964@gmail.com>
2022-07-25 20:01:53 +0530

    Background Changer

---
+++ BackgroundChanger/script.js
@@ -0,0 +1,24 @@
+function red(){
+    document.body.style.backgroundColor = 'red'
+    document.getElementById('cont').style.backgroundColor='red'
+    document.getElementById('cont').style.outline='5px solid white';
+    document.getElementById('cont').style.color='white'
+}
+function black(){
+    document.body.style.backgroundColor = 'black'
+    document.getElementById('cont').style.backgroundColor='black'
+    document.getElementById('cont').style.outline='5px solid white';
+    document.getElementById('cont').style.color='white'
+}
+function green(){
+    document.body.style.backgroundColor = 'green'
+    document.getElementById('cont').style.backgroundColor='green'
+    document.getElementById('cont').style.outline='5px solid black';
+    document.getElementById('cont').style.color='black'
+}
+function yellow(){
+    document.body.style.backgroundColor = 'yellow'
+    document.getElementById('cont').style.backgroundColor='yellow'
+    document.getElementById('cont').style.outline='5px solid black';
+    document.getElementById('cont').style.color='black'
+}
